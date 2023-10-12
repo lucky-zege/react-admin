e7ffe72d23115db5ed6693ab77cf44853c837939
lucky-zege <1833341468@qq.com>
2023-10-12 22:14:46 +0800

    暴露配置文件 解决vscode 的eslint 插件报错

---
+++ .eslintrc.js
@@ -0,0 +1,13 @@
+
+// module.exports = {
+//   parserOptions: {
+//     babelOptions: {
+//       ecmaVersion: 6,
+//       sourceType: 'module',
+//       presets: [
+//         ['babel-preset-react-app',false],
+//         'babel-preset-react-app/prod'
+//       ]
+//     }
+//   }
+// }
